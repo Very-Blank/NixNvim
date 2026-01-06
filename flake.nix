8ba631c497afd77bffe84fe300e7a5674a4fcad6
very-blank <aapeli.saarelainen.76@gmail.com>
2026-01-06 19:30:38 +0200

    feat: Should fix treesitter.

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   inputs = {
-    # FIXME: https://github.com/NotAShelf/nvf/issues/1312
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/cad22e7d996aea55ecab064e84834289143e44a0";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nvf.url = "github:NotAShelf/nvf";
     nvf.inputs.nixpkgs.follows = "nixpkgs";
